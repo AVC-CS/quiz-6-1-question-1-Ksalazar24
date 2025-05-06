48de1932619328decb8946e758818edd1a213696
ksalazar24 <kennethsalazar924@gmail.com>
2025-05-05 22:07:07 -0700

    commit 1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,48 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+// Function Prototypes
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
+int isPrime(int n); 
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    cout << "Enter two integers for the following: (begin < end): ";
+    cin >> begin >> end;
+
+    while (begin >= end) {
+        cout << "Invalid input. The first number must be less than the second.\n";
+        cout << "Enter two integers  for the following: (begin < end): ";
+        cin >> begin >> end;
+    }
+}
+
+int isPrime(int n) {
+    if (n < 2)
+        return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int getNextPrime(int begin) {
+    int num = begin + 1;
+    while (1) {
+        if (isPrime(num) == 1)
+            return num;
+        num++;
+    }
+}
+
+int getPrevPrime(int end) {
+    int num = end - 1;
+    while (num >= 2) {
+        if (isPrime(num) == 1)
+            return num;
+        num--;
+    }
+    return -1; 
+}
